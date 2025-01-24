ca96a99d6eafe36bddc5e0469d93f2252fce3a07
Christian <cmestasz@unsa.edu.pe>
2025-01-24 15:12:37 -0500

    initial

---
+++ proyecto/Singletons/SceneManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var currentScene: Node;
+var transition_rect: ColorRect;
+var gameScenes = {
+	"main_menu": preload("res://Scenes/Main/MainMenu.tscn"),
+	"main_game": preload("res://Scenes/Main/MainGame.tscn"),
+};
+
+func _setup():
+	change_scene("main_menu");
+
+func change_scene(to: String):
+	_deferred_change_scene.call_deferred(to);
+	
+func _deferred_change_scene(to: String):
+	if (currentScene):
+		currentScene.queue_free();
+	currentScene = gameScenes[to].instantiate();
+	get_tree().root.add_child(currentScene);
