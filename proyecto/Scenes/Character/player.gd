cf5d99cbd7d5a6e5fb2a1dafa5bf4ab706714852
RyanValdivia <rvaldiviase@unsa.edu.pe>
2025-01-25 16:37:54 -0500

    implementing moving camera mechanic

--- proyecto/Scenes/Character/player.gd
+++ proyecto/Scenes/Character/player.gd
@@ -3,8 +3,7 @@ extends CharacterBody2D
 @export var speed: float = 400.0
 
 func _physics_process(_delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Gets input direction
 	var direction := GameInput.process_input();
 	if direction != Vector2.ZERO:
 		velocity = direction * speed
