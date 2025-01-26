00866aae1a9610442d02acad7b49cef8abcad034
Christian <cmestasz@unsa.edu.pe>
2025-01-25 23:43:11 -0500

    add delays

--- proyecto/Singletons/dialogue_box.gd
+++ proyecto/Singletons/dialogue_box.gd
@@ -6,18 +6,17 @@ extends Control
 var is_showing: bool = false
 var dialogue_queue: Array = []
 var write_delay: float = 0.025
-var dialogue_delay: float = 1
 
 func _ready() -> void:
 	visible = false;
 	send_dialogues(
 		[
-			["Narrator", "Welcome to the game!"],
-			["Narrator", "Press the arrow keys to move around."],
-			["Narrator", "Press the space bar to jump."],
-			["Narrator", "Collect coins to increase your score."],
-			["Narrator", "Avoid the obstacles to stay alive."],
-			["Narrator", "Good luck!"]
+			["Narrator", "Welcome to the game!", 1],
+			["Narrator", "Press the arrow keys to move around.", 1],
+			["Narrator", "Press the space bar to jump.", 1],
+			["Narrator", "Collect coins to increase your score.", 1],
+			["Narrator", "Avoid the obstacles to stay alive.", 1],
+			["Narrator", "Good luck!", 1]
 		]
 	)
 
@@ -27,8 +26,8 @@ func send_dialogues(dialogues: Array) -> void:
 	if not is_showing:
 		_process_queue();
 
-func send_dialogue(p_sender: String, p_message: String) -> void:
-	dialogue_queue.append([p_sender, p_message]);
+func send_dialogue(p_sender: String, p_message: String, p_delay: float) -> void:
+	dialogue_queue.append([p_sender, p_message, p_delay]);
 	if not is_showing:
 		_process_queue();
 
@@ -37,17 +36,17 @@ func _process_queue() -> void:
 	visible = true;
 	anim_player.play("diag_in");
 	await anim_player.animation_finished;
-	
+
 	while dialogue_queue.size() > 0:
 		var current_dialogue: Array = dialogue_queue.pop_front();
-		await _process_single_dialogue(current_dialogue[0], current_dialogue[1]);
+		await _process_single_dialogue(current_dialogue[0], current_dialogue[1], current_dialogue[2]);
 
 	anim_player.play("diag_out");
 	await anim_player.animation_finished;
 	visible = false;
 	is_showing = false;
 
-func _process_single_dialogue(p_sender: String, p_message: String):
+func _process_single_dialogue(p_sender: String, p_message: String, p_delay: float) -> void:
 	sender_label.text = p_sender;
 	message_label.text = "";
 
@@ -55,4 +54,4 @@ func _process_single_dialogue(p_sender: String, p_message: String):
 		message_label.text += p_message[i];
 		await get_tree().create_timer(write_delay).timeout;
 	
-	await get_tree().create_timer(dialogue_delay).timeout;
+	await get_tree().create_timer(p_delay).timeout;
